29d3660e4a6de77759306be056a07422b0eacc61
careisch <careisch@mtu.edu>
2015-02-14 17:52:23 -0500

    what i have done so far

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+		<img src="http://team1.mis2200.sbe.mtu.edu/ACES//resources/tree line.png" class="line"></img>
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<p style="line-height:110%">Arboriculture Consulting and Education Services information:</p>
+            <p>(555) 555-5555</p>
+            <p>example@example.com</p>
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
